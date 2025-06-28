e60d8d26800897b0e3a68e0b8e8e9260d7f48f5f
rashid-exe <rashidreyaz4190@gmail.com>
2025-06-29 01:46:39 +0530

    Remove ytdl-core and switch to yt-dlp

--- backend/services/audioService.js
+++ backend/services/audioService.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 exports.downloadAndConvertAudio = async (url, id) => {
   const outputPath = path.resolve(`uploads/${id}.wav`);
-
   const command = `yt-dlp -f bestaudio -o - "${url}" | ffmpeg -i pipe:0 -ar 16000 -ac 1 -acodec pcm_s16le -f wav "${outputPath}"`;
 
   return new Promise((resolve, reject) => {
